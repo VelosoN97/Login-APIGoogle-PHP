6e4eccf82771dd230b972197e6b8d0c0b6c3f5c8
VelosoN97 <87676039+VelosoN97@users.noreply.github.com>
2025-03-22 21:50:29 -0300

    Update google-config.php

--- google-config.php
+++ google-config.php
@@ -4,8 +4,8 @@
     session_start();
 
     $client = new Google_Client();
-    $client->setClientId('336569982006-hi5ervg8lp801q6ncvf6ed1lvpsb6vcg.apps.googleusercontent.com');  
-    $client->setClientSecret('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'); 
+    $client->setClientId('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com');  
+    $client->setClientSecret('xxxxxxxxxxxxxxxxxxxxxxxxxxx'); 
     $client->setRedirectUri('http://localhost/google-login/index.php');  
     $client->addScope('email');
     $client->addScope('profile');
